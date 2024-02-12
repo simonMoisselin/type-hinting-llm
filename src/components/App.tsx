c3dbca68ee51b2c976cffd39c21b373124ca000a
Simon Moisselin (mini) <simon.moisselin@gmail.com>
2024-02-11 14:04:42 -1000

    adding a more complex start code

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,8 +7,47 @@ import { python } from '@codemirror/lang-python'
 
 import './App.css'
 
-const startCode = `def add(a, b):
-  return a + b`
+const startCode = `import pandas as pd
+import numpy as np
+from tensorflow import keras
+from tensorflow.keras import layers
+from keras.datasets import mnist
+from keras.utils import np_utils
+
+def load_data():
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    return x_train, y_train, x_test, y_test
+
+def preprocess_data(x_train, x_test):
+    x_train = x_train.reshape(x_train.shape[0], 28, 28, 1).astype('float32') / 255
+    x_test = x_test.reshape(x_test.shape[0], 28, 28, 1).astype('float32') / 255
+    return x_train, x_test
+
+def encode_labels(y_train, y_test):
+    y_train = np_utils.to_categorical(y_train, 10)
+    y_test = np_utils.to_categorical(y_test, 10)
+    return y_train, y_test
+
+def create_model():
+    model = keras.Sequential([
+        layers.Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(28, 28, 1)),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.Conv2D(64, (3, 3), activation='relu'),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.Flatten(),
+        layers.Dense(128, activation='relu'),
+        layers.Dense(10, activation='softmax')
+    ])
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
+
+def train_model(model, x_train, y_train, x_test, y_test):
+    model.fit(x_train, y_train, batch_size=128, epochs=10, verbose=1, validation_data=(x_test, y_test))
+
+def evaluate_model(model, x_test, y_test):
+    score = model.evaluate(x_test, y_test, verbose=0)
+    print('Test loss:', score[0])
+    print('Test accuracy:', score[1])`
 
 const MODELS = {
   GPT3: 'gpt-3.5-turbo-0125',
